35558c7472f6a93b0bcd5e42c11e84f125561744
najwaluthfiyah <146051052+najwaluthfiyah@users.noreply.github.com>
2023-10-26 17:51:44 +0700

    Add files via upload

---
+++ case.py
@@ -0,0 +1,25 @@
+print("""
+    *** PILIH OPERASI ***
+    1. HITUNG PERSEGI
+    2. HITUNG LINGKARAN
+    3. HITUNG SEGITIGA
+    """)
+pilihan = input("masukan pilihan: ")
+match pilihan:
+    case "1":
+        s = int(input ("masukkan sisi:"))
+        persegi = s*s
+        print("luas persegi",persegi)
+    case "2":
+        phi = 3.14
+        r = int(input ("masukkan jari:"))
+        lingkaran = phi*r*r
+        print("luas lingkaran",lingkaran)
+    case "3":
+        l = 1/2
+        a = int(input ("masukkan alas:"))
+        t = int(input ("masukkan tinggi:"))
+        segitiga = l*a*t
+        print("luas segitiga",segitiga)
+    case _:
+        print("pilihan tidak ada")
---
+++ variabel.py
@@ -0,0 +1,7 @@
+motor= ["beat", "sepeda motor","155cc" "merah", "dua roda"]
+print(motor)
+motor.append("4 juta")
+motor.append("CBS")
+print(motor) 
+motor.insert(2,"yamaha")
+print(motor)
